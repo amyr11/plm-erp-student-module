41354b6f9aae4f2f0fb0cc7f7f96479d89137f24
Dana Justine D. Pacatang <119593065+DanaPacatang@users.noreply.github.com>
2024-05-03 15:49:44 +0800

    Added credential generation page

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::view('/', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view('credential-generation', 'credential-generation')
+    ->middleware(['auth', 'verified'])
+    ->name('credential-generation');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');

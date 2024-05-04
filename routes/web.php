524863495d21db662a42800accdaf2cbf98ae37b
Dana Justine D. Pacatang <119593065+DanaPacatang@users.noreply.github.com>
2024-05-04 18:20:52 +0800

    Added Document Requests Dashboard page

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::view('/', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view('document-requests-dashboard', 'document-requests-dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('document-requests-dashboard');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');

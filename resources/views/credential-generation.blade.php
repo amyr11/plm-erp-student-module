68f2fb952e4cce044033925b14c66e5f54418b18
Dana Justine D. Pacatang <119593065+DanaPacatang@users.noreply.github.com>
2024-05-04 16:46:34 +0800

    Fixed PLM Email Email Template modal label

--- resources/views/credential-generation.blade.php
+++ resources/views/credential-generation.blade.php
@@ -233,7 +233,7 @@
                             <div
                                 class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                                 <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                                    Student Portal Account Email Template
+                                    PLM Email Credentials Email Template
                                 </h3>
                                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 
                             ms-auto inline-flex 

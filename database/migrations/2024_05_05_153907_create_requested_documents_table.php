fb0fe5e6296b07ce7b51c9fdb700546b8ed582c6
amyr11 <amyrfrancisco11@gmail.com>
2024-05-05 23:42:13 +0800

    Add RequestedDocument model and migration

---
+++ database/migrations/2024_05_05_153907_create_requested_documents_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requested_documents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('document_type_id')->constrained();
+            $table->int('no_of_copies');
+            $table->enum('status', allowed: ['Pending', 'Ready'])->default('Pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requested_documents');
+    }
+};

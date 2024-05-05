febb4a281a4efcf0f4ba253157d38053fc6549ae
amyr11 <amyrfrancisco11@gmail.com>
2024-05-06 00:21:07 +0800

    refactor: add guarded attributes in each model

--- app/Models/RequestedDocument.php
+++ app/Models/RequestedDocument.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class RequestedDocument extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 }

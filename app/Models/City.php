378c6495678220a78e0ffbb0a11f80ce97648b44
amyr11 <amyrfrancisco11@gmail.com>
2024-05-06 00:36:54 +0800

    refactor: define direct and inverse relationship for Student

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class City extends Model
 {
@@ -14,4 +15,9 @@ class City extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Student extends Model
 {
@@ -14,4 +15,24 @@ class Student extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function college(): BelongsTo
+    {
+        return $this->belongsTo(College::class);
+    }
+
+    public function degreeProgram(): BelongsTo
+    {
+        return $this->belongsTo(DegreeProgram::class);
+    }
+
+    public function birthplaceCity(): BelongsTo
+    {
+        return $this->belongsTo(City::class, 'birthplace_city_id');
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
 }

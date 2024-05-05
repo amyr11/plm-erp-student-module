947e50ddd862dc71ade809b44996aaf2791bd8a4
amyr11 <amyrfrancisco11@gmail.com>
2024-05-05 23:38:36 +0800

    Add StudentRequest model and migration

---
+++ database/migrations/2024_05_05_153456_create_student_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained()->cascadeOnDelete();
+            $table->date('date_requested');
+            $table->date('date_of_payment');
+            $table->enum('payment_mode', allowed: ['Cash', 'Online'])->default('Cash');
+            $table->int('amount_paid');
+            $table->string('receipt_no');
+            $table->string('purpose');
+            $table->date('expected_release');
+            $table->date('expected_received');
+            $table->enum('status', allowed: ['Pending', 'Ready', 'Claimed'])->default('Pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_requests');
+    }
+};

08981315807fc44c436ab81f79d6c9ff4ac55930
Dana Justine D. Pacatang <119593065+DanaPacatang@users.noreply.github.com>
2024-05-05 17:56:33 +0800

    Made header, search bar, and add button components

---
+++ resources/views/components/add-button.blade.php
@@ -0,0 +1,6 @@
+<button
+    {{ $attributes->merge(['type' => 'button', 'class' => 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-3 py-3 text-center inline-flex items-center']) }}>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M11 11v8h-2v-8H1v-2h8V1h2v8h8v2h-8z" clip-rule="evenodd" />
+    </svg>
+</button>
--- resources/views/document-requests-dashboard.blade.php
+++ resources/views/document-requests-dashboard.blade.php
@@ -1,45 +1,14 @@
 <x-app-layout>
     <!-- Header -->
-    <div class="flex items-center mt-4 mb-4">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-            <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 
-                1.06l8.69-8.689Z" />
-            <path
-                d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 
-                0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
-        </svg>
-        <h2 class="ml-2 font-medium text-gray-600">Document Requests</h2>
-    </div>
-    <!-- End Header -->
+    <?php $headerTitle = "Document Requests"; ?>
+    <x-header :title="$headerTitle" />
     <!-- Buttons Container -->
     <div class="flex items-center space-x-2.5">
         <!-- Search Bar -->
-        <form>
-            <label for="documentSearch"
-                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-            <div class="relative">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                    </svg>
-                </div>
-                <input type="search" id="documentSearch"
-                    class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search" required />
-            </div>
-        </form>
-        <!-- End Search Bar -->
+        <?php $id = "documentSearch"; ?>
+        <x-search-bar :id="$id" />
         <!-- Add button -->
-        <button id="addDocumentsButton"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-3 py-3 text-center inline-flex items-center"
-            type="button">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M11 11v8h-2v-8H1v-2h8V1h2v8h8v2h-8z" clip-rule="evenodd" />
-            </svg>
-        </button>
-        <!-- End Add button -->
+        <x-add-button id="addDocumentsButton" />
         <!-- Sort by Document type -->
         <button id="documentTypeDropdownButton" data-dropdown-toggle="documentTypeDropdown"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -201,15 +170,19 @@
                                 aria-labelledby="statusStudDropdownButton">
                                 <li>
                                     <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Processing</a>
+                                </li>
+                                <li>
+                                    <a href="#"
                                         class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Ready</a>
                                 </li>
                                 <li>
                                     <a href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Claimed</a>
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Cancelled</a>
                                 </li>
                                 <li>
                                     <a href="#"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Cancelled</a>
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Claimed</a>
                                 </li>
                             </ul>
                         </div>
@@ -243,7 +216,7 @@
                                     stroke-width="2" d="m1 1 4 4 4-4" />
                             </svg>
                         </button>
-                        <div id="tatusStudDropdown"
+                        <div id="statusStudDropdown"
                             class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                             <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
                                 aria-labelledby="statusStudDropdownButton">

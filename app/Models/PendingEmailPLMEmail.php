fba02fa2c7870e82a01e5b3735673d89207ca000
amyr11 <amyrfrancisco11@gmail.com>
2024-05-06 14:37:39 +0800

    refactor: define relationship for pending emails

--- app/Models/PendingEmailPLMEmail.php
+++ app/Models/PendingEmailPLMEmail.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PendingEmailPLMEmail extends Model
 {
@@ -14,4 +15,9 @@ class PendingEmailPLMEmail extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
 }

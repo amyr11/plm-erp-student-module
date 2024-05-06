f14b240c4d661d14f5278412cad343dc41e1b42d
amyr11 <amyrfrancisco11@gmail.com>
2024-05-06 14:35:57 +0800

    refactor: define relationship for requested documents

--- app/Models/RequestedDocument.php
+++ app/Models/RequestedDocument.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RequestedDocument extends Model
 {
@@ -14,4 +15,9 @@ class RequestedDocument extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function documentType(): BelongsTo
+    {
+        return $this->belongsTo(DocumentType::class);
+    }
 }

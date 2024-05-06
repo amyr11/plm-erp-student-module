0de76460a150f4e0b1b3a790d84ae1746401c961
amyr11 <amyrfrancisco11@gmail.com>
2024-05-06 17:22:54 +0800

    refactor: add guarded on lookup tables

--- app/Models/BiologicalSex.php
+++ app/Models/BiologicalSex.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class BiologicalSex extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 }

95483591c0112fd1d76b1715d9e04be71b8ddb8d
Dana Justine D. Pacatang <119593065+DanaPacatang@users.noreply.github.com>
2024-05-21 22:23:23 +0800

    Added requests table in Filament

--- app/Livewire/ListDocumentRequests.php
+++ app/Livewire/ListDocumentRequests.php
@@ -10,9 +10,6 @@ use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Support\Str;
-use Illuminate\Database\Eloquent\Builder;
-
 
 class ListDocumentRequests extends Component implements HasTable, HasForms
 {
@@ -26,23 +23,49 @@ class ListDocumentRequests extends Component implements HasTable, HasForms
     public function table(Table $table): Table
     {
         return $table
-            ->query(Student::query())
-            ->columns([
-                TextColumn::make('student_no')
-                    ->label('Student Number')
-                    ->sortable(),
-                TextColumn::make('last_name')
-                    ->label('Last Name'),
-                TextColumn::make('first_name')
-                    ->label('First Name'),
-                TextColumn::make('middle_name')
-                    ->label('Middle Name'),
-                TextColumn::make('personal_email')
-                    ->label('Personal Email'),
-                TextColumn::make('Status')
-                    ->default('Pending'),
-            ])
-            ->defaultSort('student_no', 'asc')
-            ->paginated(false);
+        ->query(
+            Student::query()
+                ->join('student_requests', 'students.id', '=', 'student_requests.student_id')
+                ->join('payment_modes', 'student_requests.payment_mode_id', '=', 'payment_modes.id')
+                ->join('student_request_statuses', 'student_requests.student_request_status_id', '=', 'student_request_statuses.id')
+                ->join('requested_documents', 'student_requests.id', '=', 'requested_documents.student_request_id')
+                ->join('document_types', 'requested_documents.document_type_id', '=', 'document_types.id')
+                ->select(
+                    'students.id',
+                    'students.student_no',
+                    'payment_modes.mode',
+                    'document_types.document_name',
+                    'requested_documents.no_of_copies',
+                    'student_requests.purpose',
+                    'student_requests.amount_paid',
+                    'student_requests.receipt_no',
+                    'student_requests.date_of_payment',
+                    'student_request_statuses.status'
+                )
+        )
+        ->columns([
+            TextColumn::make('student_no')
+                ->label('Student Number')
+                ->sortable(),
+            TextColumn::make('mode')
+                ->label('Payment Mode'),
+            TextColumn::make('document_name')
+                ->label('Document Type'),
+            TextColumn::make('no_of_copies')
+                ->label('Number of Copies'),
+            TextColumn::make('purpose')
+                ->label('Purpose'),
+            TextColumn::make('amount_paid')
+                ->label('Amount Paid'),
+            TextColumn::make('receipt_no')
+                ->label('Receipt Number'),
+            TextColumn::make('date_of_payment')
+                ->label('Date of Payment')
+                ->date(),
+            TextColumn::make('status')
+                ->label('Status'),
+        ])
+        ->defaultSort('student_no', 'asc')
+        ->paginated(false);
     }
 }
